b141cd4373d29642833726aa32c0b222e9a87408
maiphuocloc <44157234+maiphuocloc@users.noreply.github.com>
2020-06-07 11:55:57 +0700

    thay đổi header

--- doanphp/include/topbar.php
+++ doanphp/include/topbar.php
@@ -43,109 +43,102 @@
 <!-- top-header -->
 	<div class="agile-main-top">
 		<div class="container-fluid" style="position: fixed; z-index: 10000; padding:0px">
-			<div class="row main-top-w3l py-2">
-				<div class="col-lg-4 header-most-top">
-					
+				<div class="navbar" style="padding: 0px !important; background-color: #f7f9fb ">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light" style="float: left;" style="width: 100% !important;">
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto text-center mr-xl-5" style="margin:0px !important">
+								<li class="nav-item active mr-lg-2 mb-lg-0 mb-2">
+									<a class="nav-link" href="index.php" style="font-size: 30px; padding: 0px 10px; font-family: initial; border: 1px solid;">PHỤ KIỆN ĐIỆN THOẠI
+										<span class="sr-only">(current)</span>
+									</a>
+								</li>													
+							</ul>
+						</div>
+					</nav>
+					<nav class="navbar navbar-expand-lg navbar-light bg-light" style="float: right;">
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto text-center mr-xl-5" style="margin:0px !important">
+									<?php
+										if(isset($_SESSION['dangnhap_home']))
+										{ 
+										
+										?>
+										<li class="text-center border-right nav-item">
+											<a href="index.php?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>" class="nav-link">
+												<i class="fas fa-truck mr-2"></i>Xem đơn hàng : <?php echo $_SESSION['dangnhap_home'] ?></a>
+										</li>
+										<?php } ?>									
+										<li class="text-center border-right nav-item">
+											<a href="#" data-toggle="modal" data-target="#dangnhap" class="nav-link">
+												<i class="fas fa-sign-in-alt mr-2"></i> Đăng nhập </a>
+										</li>							
+							</ul>
+						</div>
+					</nav>
 				</div>
-				<div class="col-lg-8 header-right mt-lg-0 mt-2">
-					<!-- header lists -->
-					<ul>
 
-						<?php
-						if(isset($_SESSION['dangnhap_home'])){ 
-						
-						?>
-						<li class="text-center border-right text-white">
-							<a href="index.php?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>" class="text-white">
-								<i class="fas fa-truck mr-2"></i>Xem đơn hàng : <?php echo $_SESSION['dangnhap_home'] ?></a>
-						</li>
-						<?php
-					}
-						?>
-						
-						<li class="text-center border-right text-white">
-							<a href="#" data-toggle="modal" data-target="#dangnhap" class="text-white">
-								<i class="fas fa-sign-in-alt mr-2"></i> Đăng nhập </a>
-						</li>
-						
-					</ul>
-					<!-- //header lists -->
-				</div>
+				<div class="navbar" style="padding: 0px !important;">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100% !important;"> 
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto text-center mr-xl-5" style="margin:0px auto !important">
+									<?php 
+										$sql_category_danhmuc = mysqli_query($con,'SELECT * FROM tbl_category ORDER BY category_id DESC');
+										while($row_category_danhmuc = mysqli_fetch_array($sql_category_danhmuc)){
+									?>
+									<li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
+
+										<a class="nav-link " href="?quanly=danhmuc&id=<?php echo $row_category_danhmuc['category_id'] ?>" role="button"  aria-haspopup="true" aria-expanded="false">
+											<?php echo $row_category_danhmuc['category_name'] ?>
+										</a>
+										
+									</li>
+									<?php
+									} 
+									?>
+									<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
+										<?php
+										$sql_danhmuctin = mysqli_query($con,"SELECT * FROM tbl_danhmuc_tin ORDER BY danhmuctin_id DESC"); 
+
+										?>
+										<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											Tin tức
+										</a>
+										<div class="dropdown-menu">
+											<?php
+											while($row_danhmuctin = mysqli_fetch_array($sql_danhmuctin)){ 
+											?>
+											<a class="dropdown-item" href="?quanly=tintuc&id_tin=<?php echo $row_danhmuctin['danhmuctin_id'] ?>"><?php echo $row_danhmuctin['tendanhmuc'] ?></a>
+											<?php
+											} 
+											?>
+										</div>
+									</li>
+									<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
+										<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											Trang
+										</a>
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="product.html">Sản phẩm mới</a>
+
+											<div class="dropdown-divider"></div>
+											<a class="dropdown-item" href="checkout.html">Kiểm tra hàng</a>
+											<a class="dropdown-item" href="payment.html">Thanh toán</a>
+										</div>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="contact.html">Liên hệ</a>
+									</li>
+									<li>
+										<form class="form-inline" action="index.php?quanly=timkiem" method="POST">
+											<input class="form-control mr-sm-2" name="search_product" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search" required>
+											<button class="btn btn-success my-2 my-sm-0" name="search_button" type="submit">Tìm kiếm</button>
+										</form>	
+									</li>
+							</ul>
+						</div>
+					</nav>
 			</div>
-			<div class="navbar-inner">
-		<div class="container-fluid">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-				    aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav ml-auto text-center mr-xl-5" style="margin:0px !important">
-						<li class="nav-item active mr-lg-2 mb-lg-0 mb-2">
-							<a class="nav-link" href="index.php">Trang chủ
-								<span class="sr-only">(current)</span>
-							</a>
-						</li>
-						<?php 
-							$sql_category_danhmuc = mysqli_query($con,'SELECT * FROM tbl_category ORDER BY category_id DESC');
-							while($row_category_danhmuc = mysqli_fetch_array($sql_category_danhmuc)){
-						?>
-						<li class="nav-item  mr-lg-2 mb-lg-0 mb-2">
-
-							<a class="nav-link " href="?quanly=danhmuc&id=<?php echo $row_category_danhmuc['category_id'] ?>" role="button"  aria-haspopup="true" aria-expanded="false">
-								<?php echo $row_category_danhmuc['category_name'] ?>
-							</a>
-							
-						</li>
-						<?php
-						} 
-						?>
-						<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
-							<?php
-							$sql_danhmuctin = mysqli_query($con,"SELECT * FROM tbl_danhmuc_tin ORDER BY danhmuctin_id DESC"); 
-
-							?>
-							<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Tin tức
-							</a>
-							<div class="dropdown-menu">
-								<?php
-								while($row_danhmuctin = mysqli_fetch_array($sql_danhmuctin)){ 
-								?>
-								<a class="dropdown-item" href="?quanly=tintuc&id_tin=<?php echo $row_danhmuctin['danhmuctin_id'] ?>"><?php echo $row_danhmuctin['tendanhmuc'] ?></a>
-								<?php
-								} 
-								?>
-							</div>
-						</li>
-						<li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
-							<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Trang
-							</a>
-							<div class="dropdown-menu">
-								<a class="dropdown-item" href="product.html">Sản phẩm mới</a>
-
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="checkout.html">Kiểm tra hàng</a>
-								<a class="dropdown-item" href="payment.html">Thanh toán</a>
-							</div>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="contact.html">Liên hệ</a>
-						</li>
-						<li>
-							<form class="form-inline" action="index.php?quanly=timkiem" method="POST">
-								<input class="form-control mr-sm-2" name="search_product" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search" required>
-								<button class="btn btn-success my-2 my-sm-0" name="search_button" type="submit">Tìm kiếm</button>
-							</form>	
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	</div>
-		</div>
+		<div>
 	</div>
 	<!-- modals -->
 	<!-- log in -->
