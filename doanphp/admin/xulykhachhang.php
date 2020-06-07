b95e82175d1368ccc35f647ab4d16e83482da811
pqcuong737 <gtrax147@gmail.com>
2020-06-07 16:10:42 +0700

    them chuc nang quan ly khach hang

--- doanphp/admin/xulykhachhang.php
+++ doanphp/admin/xulykhachhang.php
@@ -8,37 +8,53 @@
 	<meta charset="UTF-8">
 	<title>Khách hàng</title>
 	<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+	<link href="../css/admin-dashboard.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="../css/Chart.css" rel="stylesheet" type="text/css" media="all" />
+
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+	<script src="../js/Chart.js"></script>
+
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <div class="collapse navbar-collapse" id="navbarNav">
-	    <ul class="navbar-nav">
-	      <li class="nav-item active">
-	        <a class="nav-link" href="xulydonhang.php">Đơn hàng <span class="sr-only">(current)</span></a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="xulydanhmuc.php">Danh mục</a>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="xulysanpham.php">Sản phẩm</a>
-	      </li>
-	         <li class="nav-item">
-	        <a class="nav-link" href="xulydanhmucbaiviet.php">Danh mục Bài viết</a>
-	      </li>
-	         <li class="nav-item">
-	        <a class="nav-link" href="xulybaiviet.php">Bài viết</a>
-	      </li>
-	       <li class="nav-item">
-	        <a class="nav-link" href="xulykhachhang.php">Khách hàng</a>
-	      </li>
-	      
-	    </ul>
-	  </div>
-	</nav><br><br>
-	<div class="container-fluid">
+	
+	
+
+
+
+<div class="container">
 		<div class="row">
-			
 			<div class="col-md-12">
+				<div class="panel panel-primary">
+
+					<div class="panel-heading">
+						<h3 class="panel-title">
+
+						</h3>
+					</div>
+
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-6 col-md-6">
+							<a href="xulydonhang.php" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Đơn hàng</a>
+							<a href="xulydanhmuc.php" class="btn btn-warning btn-lg" role="button"><span class="glyphicon glyphicon-bookmark"></span> <br/>Danh mục</a>
+							<a href="xulysanpham.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-signal"></span> <br/>Sản phẩm</a>
+							<a href="" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-comment"></span> <br/>Comments</a>
+							</div>
+							<div class="col-xs-6 col-md-6">
+							<a href="xulykhachhang.php" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-user"></span> <br/>Khách hàng</a>
+							<a href="xulydanhmucbaiviet.php" class="btn btn-info btn-lg" role="button"><span class="glyphicon glyphicon-file"></span> <br/>Loại Post </a>
+							<a href="xulybaiviet.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-picture"></span> <br/>Post</a>
+							<a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-tag"></span> <br/>Tags</a>
+							</div>
+						</div>
+
+						<div>
+						
+						<div class="col-md-12">
 				<h4>Khách hàng</h4>
 				<?php
 				$sql_select_khachhang = mysqli_query($con,"SELECT * FROM tbl_khachhang,tbl_giaodich WHERE tbl_khachhang.khachhang_id=tbl_giaodich.khachhang_id GROUP BY tbl_giaodich.magiaodich ORDER BY tbl_khachhang.khachhang_id DESC"); 
@@ -113,9 +129,29 @@
 					} 
 					?> 
 				</table>
-			</div>
-		</div>
-	</div>
+			</div> <!--col 12-->
+						
+						</div>
+						
+						
+					</div> <!--panel-body-->
+
+				</div> <!--panel-primary-->
+			</div> <!--colmd12-->
+		</div> <!--row-->
+	</div> <!--container-->
+
+
+
+
+
+	<div class="container-fluid">
+		<div class="row">
+			
+			
+
+		</div> <!--row-->
+	</div> <!--container-->
 	
 </body>
 </html>
