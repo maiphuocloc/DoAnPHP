d9de4e66192cbcde03163b9ec928095f17463cd7
pqcuong737 <gtrax147@gmail.com>
2020-06-07 13:38:02 +0700

    them dashboard cho admin

--- doanphp/admin/dashboard.php
+++ doanphp/admin/dashboard.php
@@ -19,10 +19,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Welcome Admin</title>
-	<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" /> -->
+
+	<link href="../css/admin-dashboard.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="../css/Chart.css" rel="stylesheet" type="text/css" media="all" />
+
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+	<script src="../js/Chart.js"></script>
+
+
+
 </head>
 <body>
-	<p>Xin chào : <?php echo $_SESSION['dangnhap'] ?> <a href="?login=dangxuat">Đăng xuất</a></p>
+
+	<!-- <p>Xin chào : <a href="?login=dangxuat">Đăng xuất</a></p>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	  <div class="collapse navbar-collapse" id="navbarNav">
 	    <ul class="navbar-nav">
@@ -41,6 +54,50 @@
 	      
 	    </ul>
 	  </div>
-	</nav>
+	</nav> -->
+
+	<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <span class="glyphicon glyphicon-dashboard"></span> Xin chào: <?php echo $_SESSION['dangnhap'] ?>
+					</h3>
+                </div>
+
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-6">
+                          <a href="xulydonhang.php" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Đơn hàng</a>
+                          <a href="xulydanhmuc.php" class="btn btn-warning btn-lg" role="button"><span class="glyphicon glyphicon-bookmark"></span> <br/>Danh mục</a>
+                          <a href="xulysanpham.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-signal"></span> <br/>Sản phẩm</a>
+                          <a href="" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-comment"></span> <br/>Comments</a>
+                        </div>
+                        <div class="col-xs-6 col-md-6">
+                          <a href="xulykhachhang.php" class="btn btn-success btn-lg" role="button"><span class="glyphicon glyphicon-user"></span> <br/>Khách hàng</a>
+                          <a href="xulydanhmucbaiviet.php" class="btn btn-info btn-lg" role="button"><span class="glyphicon glyphicon-file"></span> <br/>Loại Post </a>
+                          <a href="xulybaiviet.php" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-picture"></span> <br/>Post</a>
+                          <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-tag"></span> <br/>Tags</a>
+                        </div>
+                    </div>
+
+					
+					
+
+
+                    <a href="#" class="btn btn-success btn-lg btn-block" role="button"><span class="glyphicon glyphicon-globe"></span> Website khách hàng</a>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+</div>
+
 </body>
+<script>
+
+</script>
+
 </html>
