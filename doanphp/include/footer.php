2c8c812ece0f050e3b1c5bc306abcf883493f611
maiphuocloc <44157234+maiphuocloc@users.noreply.github.com>
2020-06-08 14:48:25 +0700

    thêm footer

--- doanphp/include/footer.php
+++ doanphp/include/footer.php
@@ -1,65 +1,18 @@
 
 	<!-- footer -->
-	<footer>
-		<div class="footer-top-first">
-			<div class="container py-md-5 py-sm-4 py-3">
-				<!-- footer first section -->
-				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
-				<p class="footer-main mb-4">
-					If you're considering a new laptop, looking for a powerful new car stereo or shopping for a new HDTV, we make it easy to
-					find exactly what you need at a price you can afford. We offer Every Day Low Prices on TVs, laptops, cell phones, tablets
-					and iPads, video games, desktop computers, cameras and camcorders, audio, video and more.</p>
-				<!-- //footer first section -->
-				<!-- footer second section -->
-				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
-					<div class="col-md-4 offer-footer">
-						<div class="row">
-							<div class="col-4 icon-fot">
-								<i class="fas fa-dolly"></i>
-							</div>
-							<div class="col-8 text-form-footer">
-								<h3>Miễn phí vận chuyển</h3>
-								<p>Đơn hàng trên 100$</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 offer-footer my-md-0 my-4">
-						<div class="row">
-							<div class="col-4 icon-fot">
-								<i class="fas fa-shipping-fast"></i>
-							</div>
-							<div class="col-8 text-form-footer">
-								<h3>Vận chuyển nhanh</h3>
-								<p>Toàn quốc</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 offer-footer">
-						<div class="row">
-							<div class="col-4 icon-fot">
-								<i class="far fa-thumbs-up"></i>
-							</div>
-							<div class="col-8 text-form-footer">
-								<h3>Tin cậy</h3>
-								<p>Sự đảm bảo</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- //footer second section -->
-			</div>
-		</div>
-		
-		<!-- //footer third section -->
-		
-	</footer>
-	<!-- //footer -->
-	<!-- copyright -->
-	<div class="copy-right py-3">
-		<div class="container">
-			<p class="text-center text-white">© 2018 Electro Store. All rights reserved | Design by
-				<a href="http://w3layouts.com"> W3layouts.</a>
-			</p>
-		</div>
+<footer>
+    <div class="_footer-row">
+    <div class="_footer-col-logo" >
+       
 	</div>
-	<!-- //copyright -->
+    <div class="_footer-col-info" >         
+        <h3>HỆ THỐNG PHỤ KIỆN ĐIỆN THOẠI</h3>
+        <p><i class="fa fa-map-marker" aria-hidden="true"></i> 475A, Điện Biên Phủ, quận Bình Thạnh, thành phố Hồ Chí Minh, Việt Nam</p>
+        <p><i class="fa fa-phone" aria-hidden="true"></i> 0787464929 - <i class="fa fa-fax" aria-hidden="true"></i> 0787764970</p>
+        <p><i class="fa fa-envelope" aria-hidden="true"></i> phuocloc_m@yahoo.com</p>
+	</div>       
+	</div>
+    <hr color="white"/>
+    <div class="_footer-col-logo">
+</footer>
+	
